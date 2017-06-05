7f9a8dd4d12ee87465580cb674e57198f50448b6
Derek Weber <weber.dc@gmail.com>
2017-06-05 16:23:13 +0930

    Fixed periodic output and added timestamps
    Fixed periodic output and added timestamps

--- twitter_analysis/main.py
+++ twitter_analysis/main.py
@@ -1,8 +1,14 @@
+import datetime
 import json
 import sys
 
 from lib import Options
 from lib import TwitterAnalysis
+
+
+def timestamp():
+    now = datetime.datetime.now()
+    return "%d-%02d-%02d %02d:%02d:%02d" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
 
 
 if __name__ == '__main__':
@@ -11,11 +17,22 @@ if __name__ == '__main__':
 
     print("Reading %s" % opts.tweets_file)
 
+    tweets = []
     with open(opts.tweets_file) as f:
-        tweets = [json.loads(l.rstrip('\n')) for l in f.readlines()]
+        # tweets = [json.loads(l.rstrip('\n')) for l in f.readlines()]
+        lines = f.readlines()
+        batch_size = (len(lines) / 10)
+        count = 0
+        for l in lines:
+            count += 1
+            if int(count % batch_size) == 0:
+                print("[%s] Read %d lines..." % (timestamp(), count))
+            tweets.append(json.loads(l.rstrip('\n')))
 
-    print("Read %d tweets" % len(tweets))
+    print("[%s] Read %d tweets" % (timestamp(), len(tweets)))
 
     analyser = TwitterAnalysis(opts)
 
     analyser.analyse(tweets)
+
+    print("Finished at %s" % timestamp())
